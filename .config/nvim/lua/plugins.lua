4e866a59ccfb4dc34234bf7449163ecac36c8227
Anand Mahamuni <m.anand7203@gmail.com>
2023-12-21 09:05:25 +0530

    fug add

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,7 +1,5 @@
 return {
-
 	{'numToStr/Comment.nvim',opts = {},lazy = false,},
-	
 	{"nvim-tree/nvim-web-devicons"}, 
 	{"MunifTanjim/nui.nvim"},
 }
--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,10 +1,8 @@
-return {
-	"folke/tokyonight.nvim",
-	lazy = false,
-	priority = 1000,
-	opts = {},
+return { 
+	"rose-pine/neovim",
+	name = "rose-pine", 
+	priority = 1000, 
 	config = function()
-		vim.cmd.colorscheme "tokyonight"
+		vim.cmd.colorscheme "rose-pine"
 	end
 }
-
