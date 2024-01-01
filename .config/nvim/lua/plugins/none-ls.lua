393050580e143f2ab10cb7fca48b1cb26a805cbf
Anand Mahamuni <m.anand7203@gmail.com>
2024-01-01 22:28:42 +0530

    Added formatter for python

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -6,9 +6,9 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.black,
 				null_ls.builtins.diagnostics.eslint_d,
-				null_ls.builtins.diagnostics.rubocop,
-				null_ls.builtins.formatting.rubocop,
 			},
 		})
 
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,31 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end
-  }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				auto_install = true,
+				ensure_installed = {
+					"rust",
+					"python",
+					"bash",
+					"comment",
+					"css",
+					"html",
+					"javascript",
+					"jsdoc",
+					"jsonc",
+					"lua",
+					"markdown",
+					"regex",
+					"toml",
+					"typescript",
+					"yaml",
+				},
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
 }
