cd71582b5cdbb811a8547f9f44ae6c7d5d1c2856
Anand Mahamuni <m.anand7203@gmail.com>
2024-01-10 19:59:59 +0530

    Added the correct remap for fug.

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -2,6 +2,6 @@ return {
 	"tpope/vim-fugitive",
 	config = function()
 		vim.keymap.set("n","<leader>gs",vim.cmd.Git,{})
-		vim.keymap.set("n","P",vim.cmd.Git,{})
+		vim.keymap.set("n","P",":Git push",{})
 	end,
 }
