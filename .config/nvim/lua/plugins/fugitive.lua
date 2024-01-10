0e7590b6bd735653d70d9fc76d005ca562dde742
Anand Mahamuni <m.anand7203@gmail.com>
2024-01-10 20:00:45 +0530

    Correction again!

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -2,6 +2,6 @@ return {
 	"tpope/vim-fugitive",
 	config = function()
 		vim.keymap.set("n","<leader>gs",vim.cmd.Git,{})
-		vim.keymap.set("n","P",":Git push",{})
+		vim.keymap.set("n","P",":Git push<CR>",{})
 	end,
 }
