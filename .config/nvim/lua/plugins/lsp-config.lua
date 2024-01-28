993f2f2f2f7d9563351baf4cd65c21fc702473cd
Anand Mahamuni <m.anand7203@gmail.com>
2024-01-28 13:37:18 +0530

    Awesome lsp-config keymaps!

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -37,9 +37,11 @@ return {
 			})
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {})
+			vim.keymap.set("n", "<leader>df", vim.diagnostic.goto_next, {})
 		end,
 	},
 }
