f50eca12ff8faa8a791b7fc40763351189f4a98b
Anand Mahamuni <m.anand7203@gmail.com>
2024-02-18 21:37:39 +0530

    Trouble.nvim added

---
+++ .config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,9 @@
+return {
+	"folke/trouble.nvim",
+	config = function ()
+		vim.keymap.set("n","<leader>xx","<cmd>TroubleToggle<CR>")
+		vim.keymap.set("n","<leader>xw","<cmd>TroubleToggle workspace_diagnostics<CR>")
+		vim.keymap.set("n","<leader>xd","<cmd>TroubleToggle document_diagnostics<CR>")
+		vim.keymap.set("n","<leader>xq","<cmd>TroubleToggle quickfix<CR>")
+	end
+}
