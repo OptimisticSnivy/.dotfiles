19a1d0aed6049a4b6a319850cd755159d82df497
Anand Mahamuni <m.anand7203@gmail.com>
2024-03-05 00:19:39 +0530

    curlwalls clean

--- scripts/curlwalls.sh
+++ scripts/curlwalls.sh
@@ -2,10 +2,11 @@
 
 proto="$(echo $1 | grep :// | sed -e's,^\(.*://\).*,\1,g')"
 url="$(echo ${1/$proto/})"
-format="$(echo $url | grep -o '...$')"
 
 filename="$(echo $url | awk -F "/" '{ print $NF}')"
 
-
 curl $url > ~/repos/wallpapers/$filename
 echo -e "filename: $filename"
+
+# To-do:-
+# Parse only if jpg or png
