206ae00c9956348f3264a60b371564f07f32ddc6
Anand Mahamuni <m.anand7203@gmail.com>
2024-07-08 19:15:29 +0530

    Catppuccin hell yeah!

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,22 @@
 return {
-	"rose-pine/neovim",
+	"catppuccin/nvim",
 	version = false,
 	lazy = false,
 	priority = 1000, -- make sure to load this before all the other start plugins
 	-- Optional; default configuration will be used if setup isn't called.
 	config = function()
-		require("rose-pine").setup({
-			styles = {
-				bold = true,
-				italic = true,
-				transparency = true,
+		require("catppuccin").setup({
+			flavour = "auto", -- latte, frappe, macchiato, mocha
+			background = { -- :h background
+				light = "latte",
+				dark = "mocha",
 			},
+			styles = {
+				comments = { "italic" }, -- Change the style of comments
+				conditionals = { "italic" },
+			},
+			transparent_background = true, -- disables setting the background color.
 		})
-		vim.cmd.colorscheme("rose-pine")
+		vim.cmd.colorscheme("catppuccin")
 	end,
 }
