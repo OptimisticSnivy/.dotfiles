722b317258433b54225a40acbe085848f6a8d786
Anand Mahamuni <m.anand7203@gmail.com>
2024-07-11 00:06:43 +0530

    Cleanup

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,22 +1,33 @@
 return {
-	"catppuccin/nvim",
+	"ellisonleao/gruvbox.nvim",
 	version = false,
 	lazy = false,
-	priority = 1000, -- make sure to load this before all the other start plugins
-	-- Optional; default configuration will be used if setup isn't called.
+	priority = 1000,
 	config = function()
-		require("catppuccin").setup({
-			flavour = "auto", -- latte, frappe, macchiato, mocha
-			background = { -- :h background
-				light = "latte",
-				dark = "mocha",
+		require("gruvbox").setup({
+			terminal_colors = true, -- add neovim terminal colors
+			undercurl = true,
+			underline = true,
+			bold = true,
+			italic = {
+				strings = true,
+				emphasis = true,
+				comments = true,
+				operators = false,
+				folds = true,
 			},
-			styles = {
-				comments = { "italic" }, -- Change the style of comments
-				conditionals = { "italic" },
-			},
-			transparent_background = true, -- disables setting the background color.
+			strikethrough = true,
+			invert_selection = false,
+			invert_signs = false,
+			invert_tabline = false,
+			invert_intend_guides = false,
+			inverse = true, -- invert background for search, diffs, statuslines and errors
+			contrast = "", -- can be "hard", "soft" or empty string
+			palette_overrides = {},
+			overrides = {},
+			dim_inactive = false,
+			transparent_mode = true,
 		})
-		vim.cmd.colorscheme("catppuccin")
+		vim.cmd.colorscheme("gruvbox")
 	end,
 }
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -6,7 +6,7 @@ return {
 		end
 		require("lualine").setup({
 			options = {
-				theme = 'rose-pine',
+				theme = 'gruvbox',
 				component_separators = { left = '', right = '' },
 				section_separators = { left = '', right = '' },
 			},
