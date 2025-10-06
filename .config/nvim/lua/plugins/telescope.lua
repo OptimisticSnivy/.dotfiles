634c3a66f3b10251d7623091956f7d89ea13e9bf
Anand Mahamuni <optimisticsnivy7203@proton.me>
2025-10-06 19:38:31 +0530

    Cleanup

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,8 +3,7 @@ return {
 	tag = "0.1.5",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
-		-- requires ripgrep to be installed
-		file_ignore_patterns = { "node%_modules/.*" } 
+		file_ignore_patterns = { "node%_modules/.*", "lib/" } -- requires ripgrep to be installed
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
