7b93dabadc0f52fe620b3c672c44d71963dfa983
Anand Mahamuni <optimisticsnivy7203@proton.me>
2025-10-24 20:34:00 +0530

    Rewrite begins with kickstarter.nvim

--- .config/nvim/lua/plugins.lua
+++
@@ -1,5 +0,0 @@
-return {
-	{ "numToStr/Comment.nvim", opts = {}, lazy = false },
-	"nvim-tree/nvim-web-devicons",
-	"tpope/vim-sleuth",
-}
--- .config/nvim/lua/plugins/noice.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"folke/noice.nvim",
-	event = "VeryLazy",
-	opts = {},
-	dependencies = {
-		"MunifTanjim/nui.nvim",
-	},
-	vim.keymap.set("n", "<leader>ds", "<CMD>NoiceDismiss<CR>", { desc = "Dismiss all Notifs" }),
-}
