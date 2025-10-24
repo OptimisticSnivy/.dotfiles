9f3c9b4c15fafe01839ec5eb36a40fe3d0019e07
Anand Mahamuni <optimisticsnivy7203@proton.me>
2025-10-24 23:18:15 +0530

    General Cleanup

--- scripts/boost.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if grep -q 0 /sys/devices/system/cpu/cpufreq/boost; then 
-    echo "1" | sudo tee /sys/devices/system/cpu/cpufreq/boost
-else
-    echo "0" | sudo tee /sys/devices/system/cpu/cpufreq/boost
-fi
