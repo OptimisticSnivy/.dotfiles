113876c00935739c5efa862110836a3401324ac6
Anand Mahamuni <optimisticsnivy7203@proton.me>
2025-12-07 13:15:04 +0530

    Ghostty gets removed

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,19 @@
 return {
-	"savq/melange-nvim",
+	"olivercederborg/poimandres.nvim",
+	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.cmd.colorscheme("melange")
+		require("poimandres").setup({
+			bold_vert_split = false, -- use bold vertical separators
+			dim_nc_background = false, -- dim 'non-current' window backgrounds
+			disable_background = false, -- disable background
+			disable_float_background = false, -- disable background for floats
+			disable_italics = false, -- disable italics
+		})
+	end,
+
+	-- optionally set the colorscheme within lazy config
+	init = function()
+		vim.cmd("colorscheme poimandres")
 	end,
 }
