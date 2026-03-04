31417c48e2cde9a156106a5ca515e49556fa6cd2
Anand Mahamuni <optimisticsnivy7203@proton.me>
2026-03-04 15:35:09 +0530

    Hi Neovim, Again!

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,18 +1,10 @@
 return {
-	"olivercederborg/poimandres.nvim",
+	"savq/melange-nvim",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		require("poimandres").setup({
-			bold_vert_split = false, -- use bold vertical separators
-			disable_background = true, -- disable background
-			disable_float_background = true, -- disable background for floats
-			disable_italics = false, -- disable italics
-		})
-	end,
-
-	-- optionally set the colorscheme within lazy config
-	init = function()
-		vim.cmd("colorscheme poimandres")
+		vim.cmd.colorscheme("melange")
+		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 	end,
 }
